4ce35521645ccda5fe472888d877a37338ba6b47
manny1040 <mabercian13@gmail.com>
2025-04-23 09:53:28 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,26 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
-} 
+ private int s;
+punlic StepTracker(int s1){
+ s = s1;
+}
+public int addDailysSteps(int spd){
+ int total = 0;
+ int ad = 0;
+ int d = 0;
+ total = total + spd;
+ if(spd >= s)
+   ad++;
+   d++;
+ else 
+   d++;
+}
+public int activeDays()}
+ return ad;
+}
+public double averageSteps(){
+  return (double)total/d;
+ }
+}
+ 
